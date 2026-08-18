a19368060a6f672d70377a615130b1e161074f00
HB9IIU <daniel.staempfli@gmail.com>
2026-08-18 10:32:34 +0100

    Rename notice to "POOR SIGNAL" - names the cause, not just the symptom

--- qo100_sdl/src/main.cpp
+++ qo100_sdl/src/main.cpp
@@ -1945,7 +1945,7 @@ void draw_video_notice(TextCache & text, const SDL_Rect & bounds, VideoNotice no
             message = "WAIT - TUNING";
             break;
         case VideoNotice::NoVideoStream:
-            message = "POOR";
+            message = "POOR SIGNAL";
             colour = kRed;
             break;
         case VideoNotice::None:
